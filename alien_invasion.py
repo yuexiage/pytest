24bf04c65e9c567d2669edc46442f04330ea7f6f
yuexiage <418221610@qq.com>
2019-05-26 13:18:13 +0800

    外星人大战左右移动完成

---
+++ alien_invasion.py
@@ -0,0 +1,27 @@
+import sys
+import pygame
+from ship import Ship
+import game_functions as gf
+from settings import Settings
+
+def run_game():
+    # 初始化游戏并创建一个屏幕对象
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption(ai_settings.window_title)
+
+    # 创建一艘飞船
+    ship = Ship(screen)
+
+    # 开始游戏的主循环
+    while True:
+        # 监视键盘和鼠标事件
+        gf.check_events(ship)
+
+        # 更新飞船位置
+        ship.update()
+
+        # 每次循环时都重绘屏幕
+        gf.update_screen(ai_settings, screen, ship)
+run_game()
